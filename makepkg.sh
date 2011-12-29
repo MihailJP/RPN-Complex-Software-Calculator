50c3f47174e422403ac46d9ba4aa3ae18aebb62b
TANIGAWA Takashi <mihailjp@gmail.com>
2011-12-29 23:34:19 +0900

    makepkg.sh更新、パーミッションをいじる(Windows上では意味なし？)

--- makepkg.sh
+++ makepkg.sh
@@ -5,4 +5,6 @@ mkdir release&&mkdir release/rpncalc
 
 cp rpncalc/bin/Release/rpncalc.exe rpncalc/bin/Release/complex.dll README README.ja License release/rpncalc
 cd release
+chmod 755 rpncalc/rpncalc.exe
+chmod 644 rpncalc/complex.dll
 7z a -mx=9 rpncalc.zip rpncalc
