4845eb6b693f690ca480897ccecd85911df53f85
TANIGAWA Takashi <mihailjp@gmail.com>
2012-02-06 20:55:49 +0900

    バージョン番号を変更

--- complex/My Project/AssemblyInfo.vb
+++ complex/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.3.0.0")> 
-<Assembly: AssemblyFileVersion("0.3.0.0")> 
+<Assembly: AssemblyVersion("0.4.0.0")> 
+<Assembly: AssemblyFileVersion("0.4.0.0")> 
