bc41b8f3e78a508000c27ebd9565b0af9dbf8667
TANIGAWA Takashi <mihailjp@gmail.com>
2012-02-06 23:05:54 +0900

    バージョン番号を変更

--- rpncalc/My Project/AssemblyInfo.vb
+++ rpncalc/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.4.0.0")> 
+<Assembly: AssemblyVersion("0.4.1.0")> 
 <Assembly: AssemblyFileVersion("0.2.0.0")> 
